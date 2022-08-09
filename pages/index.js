5c65b64931755b3f9321b30502268f0f2ae74b5a
Sarut <airwavy.442511@gmail.com>
2022-08-09 20:12:39 +0700

    ui upgrade v.1.0

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Link from 'next/link'
 import { useState, useEffect, useRef} from "react"
+import Primogem from "./../access/primogem.png"
+import Interwined from "./../access/interwined.png"
+import Image from "next/image"
 
 export default function IndexPage() {
   const chanceFourStar = 13
@@ -26,40 +29,49 @@ export default function IndexPage() {
   const [fourWish, setFourWish] = useState(0)
   const [lostWish, setLostWish] = useState(0)
 
-  const wishes = () => {
+  const wishes = (confirm) => {
 
-    let wishedGlobal = Math.floor(Math.random() * 100)
+    let wishedGlobal;
+
+    if (confirm) {
+      wishedGlobal = 0.624
+    } else {
+      wishedGlobal = Math.random() * 100
+    }
+
     setFiveWish(wishedGlobal)
-
+    console.log(wishedGlobal)
     if (pity === confirmFiveStar) { // ถึงการันตี 5 ดาว
-      let lostWishes = Math.floor(Math.random() * 100)
+      let lostWishes = Math.random() * 100
       setLostWish(lostWishes)
-      if (lostWishes < 50) { // 2
+      if (lostWishes < 50 || lost) { // 2
 
-        setResult(result => [...result, "Win 50/50 (5 stars)"])
+        setResult(result => [...result, <h4 className="win" key="win">Win 50/50 (5 stars / guarantee)</h4>])
         setPity(0)
+        setLost(false)
         return;
       }
 
       setLost(true)
-      setResult(result => [...result, "Lost 50/50 (5 stars)"])
+      setResult(result => [...result, <h4 className="lost" key="lost">Lost 50/50 (5 stars / guarantee)</h4>])
       setPity(0)
       // 1
       return;
     }
 
     if (wishedGlobal <= chanceFiveStar) { // ออก 5 ดาว
-      let lostWishes = Math.floor(Math.random() * 100)
+      let lostWishes = Math.random() * 100
       setLostWish(lostWishes)
-      if (lostWishes < 50) { // 2
+      if (lostWishes < 50 || lost) { // 2
 
-        setResult(result => [...result, "Win 50/50 (5 stars)"])
+        setResult(result => [...result, <h4 className="win" key="win">Win 50/50 (5 stars)</h4>])
         setPity(0)
+        setLost(false)
         return;
       }
 
       setLost(true)
-      setResult(result => [...result, "Lost 50/50 (5 stars)"])
+      setResult(result => [...result, <h4 className="lost" key="lost">Lost 50/50 (5 stars)</h4>])
       setPity(0)
       // 1
       return;
@@ -67,20 +79,20 @@ export default function IndexPage() {
 
     // 4 STAR
     if (fourStarPity < confirmFourStar) { // ไม่ถึงการันตี 4 ดาว
-      let wishedFourStar = Math.floor(Math.random() * 100)
+      let wishedFourStar = Math.random() * 100
       setFourWish(wishedFourStar)
 
       if (wishedFourStar <= chanceFourStar) { // 4
         console.log("4star")
         setFourStarPity(0)
-        setResult(result => [...result, "have some 4 stars"])
+        setResult(result => [...result, <h4 className="four" key="four">have some 4 stars</h4>])
         //setPity(pity + 1)
         return;
       } // 4 
 
       console.log("3star")
       setFourStarPity(fourStarPity => fourStarPity + 1)
-      setResult(result => [...result, "have some 3 stars"])
+      setResult(result => [...result, <p className="common" key="common">have some 3 stars</p>])
       //setPity(pity + 1)
       return;
     }
@@ -89,7 +101,7 @@ export default function IndexPage() {
     // ถ้ามีการันตี 4 ดาว
     if (fourStarPity === confirmFourStar) {
       console.log("4starcon")
-      setResult(result => [...result, "have some 4 stars (guarantee)"])
+      setResult(result => [...result, <h4 className="fourgua" key="fourgua">have some 4 stars (guarantee)</h4>])
       //setPity(pity + 1)
       setFourStarPity(0)
       return;
@@ -98,11 +110,11 @@ export default function IndexPage() {
   }
 
 
-  const pulls = (times) => {
+  const pulls = (times, confirm) => {
     // setResult([])
     if (times === 1) {
       setPity(pity => pity + 1)
-      wishes()
+      wishes(confirm)
     } else {
       for (let i = 0; i < 10; i++) {
         setPity(pity => pity + 1)
@@ -117,9 +129,11 @@ export default function IndexPage() {
   }, [pity])
 
   return (
-    <div>
+    <div className="content">
       <div>
-        <h3>Pity: {pity}</h3>
+        <h4>Wish</h4>
+        <span><Image className="gem" width={20} height={20} src={Primogem}/> -{pity * 160}</span>
+        <span><Image className="interwined" width={20} height={20} src={Interwined}/> -{pity}</span>
         <h1>Results:</h1>
         <ul>
           {result.length > 0 ? result.map((val, key) => (
@@ -130,17 +144,32 @@ export default function IndexPage() {
           }
         </ul>
       </div>
-      <button onClick={() => pulls(1)}>Wishes x1</button>
-      <button onClick={() => pulls(10)} disabled>Wishes x10 (บัคอยู่)</button>
+      <div className="pulling">
+        <button onClick={() => pulls(1, false)}>
+          <label>Wish x1</label>
+          <br />
+          <Image className="primogem" width={20} height={20} src={Primogem}/> x 160
+        </button>
+        <button onClick={() => pulls(10, false)} disabled>
+          <label>Wish x10</label>
+          <br />
+          <Image className="primogem" width={20} height={20} src={Primogem}/> x 1600
+        </button>
+      </div>
       <br />
       <div>
         <h4>Admin zone</h4>
+        <button onClick={() => pulls(1, true)}>
+          <label>Confirm 5-star</label>
+          <br />
+          <Image className="primogem" width={20} height={20} src={Primogem}/> x 0
+        </button>
         <p>5 stars random chance : {fiveWish}</p>
         <p>4 stars random chance : {fourWish}</p>
         <p>win 5 stars 50/50 : {lostWish}</p>
         <p>4 stars guarantee : {fourStarPity}/10</p>
-        <p>Primogems eaten : {pity * 160}</p>
       </div>
+      <p className="right">This gacha system based from Genshin Impact from HoYoVerse</p>
     </div>
   )
 }
